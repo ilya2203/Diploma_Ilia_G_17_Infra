1d93e0f5320e4108af68dd5ea774077c4537ed23
Ilia Gozhin <ilia_gozhin@epam.com>
2021-08-11 15:26:52 +0300

    New version of main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,29 +1,29 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.56"
+      version = ">=2.56"   
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "epm-rdsp"
+backend "azurerm" {
+    resource_group_name  = "__azureRG__"
     storage_account_name = "__azureStorageAccount__"
       cointainer_name    = "terraform"
       key                = "terraform.tfstate"
     access_key = "__storagekey__"
-    features{}
-  }
+    }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_storage_account" "gozhin" {
   name                     = "gozhin02"
-  resource_group_name      = "epm-rdsp"
+  resource_group_name      = "__azureRG__"
   location                 = "West Europe"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
   tags = {
     owner = "ilia_gozhin@epam.com"
   }
